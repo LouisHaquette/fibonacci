b0042171ead08101325e064dd2c04919fc74a768
LouisHaquette <louis@alphee.fr>
2015-01-29 21:53:12 +0100

    Create fibonacci.h

---
+++ fibonacci.h
@@ -0,0 +1,11 @@
+#ifndef FIBONACCI_H_INCLUDED
+#define FIBONACCI_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void fibonacci_computing(unsigned int *lastTerm, unsigned int *oldTerm);
+
+void fibonacci(unsigned int nbr_terms);
+
+#endif // FIBONACCI_H_INCLUDED
